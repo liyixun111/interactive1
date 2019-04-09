ae083ce92ae2c7c8404dca413570e7a754c7e301
liyixun <liyixun@cca.edu>
2019-04-09 06:07:01 -0700

    changes

---
+++ P1/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $(".grid-item").click(function () {
+        $(".Game1, .Game1 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item1").click(function () {
+        $(".Game2,.Game2 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item2").click(function () {
+        $(".Game3,.Game3 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item3").click(function () {
+        $(".Game4,.Game4 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item4").click(function () {
+        $(".Game5,.Game5 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item5").click(function () {
+        $(".Game6,.Game6 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item6").click(function () {
+        $(".Game7,.Game7 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item7").click(function () {
+        $(".Game8,.Game8 .Description").toggleClass('appear');
+    });
+    $(".grid-item8").click(function () {
+        $(".Game9,.Game9 .Description").toggleClass('appear');
+    });
+    $(".grid-item9").click(function () {
+        $(".Game10,.Game10 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item10").click(function () {
+        $(".Game11,.Game11 .Description").toggleClass('appear');
+    });
+    $(".grid-item11").click(function () {
+        $(".Game12,.Game12 .Description").toggleClass('appear');
+    });
+
+    $(".grid-item12").click(function () {
+        $(".Game13,.Game13 .Description").toggleClass('appear');
+    });
+});
