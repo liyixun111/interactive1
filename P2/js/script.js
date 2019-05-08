31ecc047020af6310cc982d6e847c3cae24909b5
liyixun <liyixun@cca.edu>
2019-05-08 07:03:21 -0700

    changes

---
+++ P2/js/script.js
@@ -0,0 +1,122 @@
+$(document).ready(function () {
+    $("#a").hover(function () {
+        $("#a .filled").addClass('black');
+    });
+    $("#b").hover(function () {
+        $("#b .filled").addClass('black');
+    });
+    $("#c").hover(function () {
+        $("#c .filled").addClass('black');
+    });
+    $("#d").hover(function () {
+        $("#d .filled").addClass('black');
+    });
+    $("#e").hover(function () {
+        $("#e .filled").addClass('black');
+    });
+    $("#f").hover(function () {
+        $("#f .filled").addClass('black');
+    });
+    $("#g").hover(function () {
+        $("#g .filled").addClass('black');
+    });
+    $("#h").hover(function () {
+        $("#h .filled").addClass('black');
+    });
+    $("#i").hover(function () {
+        $("#i .filled").addClass('black');
+    });
+    $("#j").hover(function () {
+        $("#j .filled").addClass('black');
+    });
+    $("#k").hover(function () {
+        $("#k .filled").addClass('black');
+    });
+    $("#l").hover(function () {
+        $("#l .filled").addClass('black');
+    });
+    $("#m").hover(function () {
+        $("#m .filled").addClass('black');
+    });
+    $("#n").hover(function () {
+        $("#n .filled").addClass('black');
+    });
+    $("#o").hover(function () {
+        $("#o .filled").addClass('black');
+    });
+    $("#p").hover(function () {
+        $("#p .filled").addClass('black');
+    });
+    $("#q").hover(function () {
+        $("#q .filled").addClass('black');
+    });
+    $("#r").hover(function () {
+        $("#r .filled").addClass('black');
+    });
+    $("#s").hover(function () {
+        $("#s .filled").addClass('black');
+    });
+    $("#t").hover(function () {
+        $("#t .filled").addClass('black');
+    });
+    $("#u").hover(function () {
+        $("#u .filled").addClass('black');
+    });
+    $("#v").hover(function () {
+        $("#v .filled").addClass('black');
+    });
+    $("#w").hover(function () {
+        $("#w .filled").addClass('black');
+    });
+    $("#x").hover(function () {
+        $("#x .filled").addClass('black');
+    });
+    $("#y").hover(function () {
+        $("#y .filled").addClass('black');
+    });
+    $("#z").hover(function () {
+        $("#z .filled").addClass('black');
+    });
+    $("#number0").hover(function () {
+        $("#number0 .filled").addClass('black');
+    });
+    $("#number1").hover(function () {
+        $("#number1 .filled").addClass('black');
+    });
+    $("#number2").hover(function () {
+        $("#number2 .filled").addClass('black');
+    });
+    $("#number3").hover(function () {
+        $("#number3 .filled").addClass('black');
+    });
+    $("#number4").hover(function () {
+        $("#number4 .filled").addClass('black');
+    });
+    $("#number5").hover(function () {
+        $("#number5 .filled").addClass('black');
+    });
+    $("#number6").hover(function () {
+        $("#number6 .filled").addClass('black');
+    });
+    $("#number7").hover(function () {
+        $("#number7 .filled").addClass('black');
+    });
+    $("#number8").hover(function () {
+        $("#number8 .filled").addClass('black');
+    });
+    $("#number9").hover(function () {
+        $("#number9 .filled").addClass('black');
+    });
+    $("#question").hover(function () {
+        $("#question .filled").addClass('black');
+    });
+    $("#exclamation").hover(function () {
+        $("#exclamation .filled").addClass('black');
+    });
+    $("#quotation").hover(function () {
+        $("#quotation .filled").addClass('black');
+    });
+    $("#cp").hover(function () {
+        $("#cp .filled").addClass('black');
+    });
+});
